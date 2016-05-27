b69a903bb606288da5781ad6dacc0bc1f8df28cd
Wuwei Lin <vincentl13x@gmail.com>
2016-05-27 19:06:09 +0800

    Fix exceptions

--- Exception.h
+++ Exception.h
@@ -1,3 +1,7 @@
+#ifndef _BASIC_EXCEPTION
+#define _BASIC_EXCEPTION
+
+
 #include <exception>
 #include <string>
 
@@ -5,32 +9,35 @@ struct BasicException : public std::exception {
 };
 
 struct DivideByZeroException : public BasicException {
-    virtual const char *what() const {
+    virtual const char *what() const noexcept {
         return "DIVIDE BY ZERO";
     }
 };
 
 struct InvalidNumberException : public BasicException {
-    virtual const char *what() const {
+    virtual const char *what() const noexcept {
         return "INVALID NUMBER";
     }
 };
 
 struct VariableNotDefinedException : public BasicException {
-    virtual const char *what() const {
+    virtual const char *what() const noexcept {
         return "VARIABLE NOT DEFINED";
     }
 };
 
 struct LineNumberErrorException : public BasicException {
-    virtual const char *what() const {
+    virtual const char *what() const noexcept {
         return "LINE NUMBER ERROR";
     }
 };
 
 struct SyntaxErrorException : public BasicException {
-    virtual const char *what() const {
+    virtual const char *what() const noexcept {
         return "SYNTAX ERROR";
     }
 };
+
+
+#endif
 
