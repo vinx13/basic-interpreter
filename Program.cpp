049d06fc798d72f4b9b08479b5649d0f5e18c9ee
Wuwei Lin <vincentl13x@gmail.com>
2016-05-28 11:38:32 +0800

    Fix jumpTo

--- Program.cpp
+++ Program.cpp
@@ -1,29 +1,30 @@
 #include <string>
-#include <utility>
 #include <iostream>
 #include "Program.h"
-#include "TokenStream.h"
-#include "Statement.h"
+#include "Exception.h"
 
 void Program::addStatement(const std::string &source) {
     TokenStream ts(source);
     auto lineno = ts.read(kTokenType::Number);
     statements_[std::stoi(lineno->value)] = std::make_pair(
-        source,
-        Statement::parse(ts)
-    ); 
+            source,
+            Statements::parse(ts)
+    );
 
 }
 
 void Program::removeStatement(int line) {
     auto iter = statements_.find(line);
-    if(iter != statements_.end()) {
+    if (iter != statements_.end()) {
         statements_.erase(iter);
     }
 }
 
 void Program::jumpTo(int lineno) {
     cur_line_ = lineno;
+    if (statements_.find(lineno) == statements_.end()) {
+        throw LineNumberErrorException();
+    }
 }
 
 void Program::clear() {
@@ -37,12 +38,12 @@ void Program::stop() {
 void Program::run() {
     auto iter = statements_.begin();
     stopped_ = false;
-    
-    while(!stopped_ && iter != statements_.end()) {
+
+    while (!stopped_ && iter != statements_.end()) {
         cur_line_ = iter->first;
         iter->second.second->execute();
-        
-        if(cur_line_ != iter->first) { //jumpTo is called
+
+        if (cur_line_ != iter->first) { //jumpTo is called
             iter = statements_.find(cur_line_);
         } else {
             ++iter;
@@ -51,9 +52,8 @@ void Program::run() {
 }
 
 void Program::printStatements() const {
-    for(auto iter = statements_.begin(); iter != statements_.end(); ++iter) {
-        std::cout << iter->second.first << std::endl;
+    for (const auto &stmt_pair: statements_) {
+        std::cout << stmt_pair.second.first << std::endl;
     }
-    std::cout << std::endl;
 }
 
